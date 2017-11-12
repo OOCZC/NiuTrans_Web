e8d75d2fcd565805f5e881406ff5bb51971f5219
OOC <whatisooc@gmail.com>
2017-11-13 00:41:26 +0800

    1.0.0 版本

--- application.py
+++ application.py
@@ -4,16 +4,18 @@ app = Flask(__name__)
 
 @app.route('/trans', methods=['POST'])
 def fanyi():
-    yuanwen = request.form['yuanwen']
-    print("fanyi()", yuanwen, "***")
-    return "123456"
+    import mt
+    transText = mt.mtChinese(request.form['yuanwen'])
+    print("fanyi()", transText, "***")
+    return transText
 
 @app.route('/')
 def index():
     print("index()")
     return render_template('index.html')
 
+
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=6464)
     #外部可访问。  app.run() #外部不可访问
