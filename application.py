442a40cd0e4fbb1251291c33ef833b8ac105bf6a
OOC <whatisooc@gmail.com>
2017-11-12 23:56:14 +0800

    Update index.html and application.py

--- application.py
+++ application.py
@@ -2,9 +2,18 @@ from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
 
+@app.route('/trans', methods=['POST'])
+def fanyi():
+    yuanwen = request.form['yuanwen']
+    print("fanyi()", yuanwen, "***")
+    return "123456"
+
 @app.route('/')
 def index():
+    print("index()")
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.debug = True
+    app.run(host='0.0.0.0', port=80)
+    #外部可访问。  app.run() #外部不可访问
