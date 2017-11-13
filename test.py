03bebec6e2dbdf8dab57e31b13f2bb82f2410dbd
OOC <whatisooc@gmail.com>
2017-11-13 16:55:41 +0800

    Crawler test

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
-import mt
+import bs4
+def get_html(url):
+    import urllib.request
+    req = urllib.request.Request(url)
+    #req.add_header('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
+    #req.add_header('User-Agent', '')
+    try:
+        with urllib.request.urlopen(req,\
+         timeout = 10) as html:
+            return html.read().decode()
+    except:
+        print('Error')
+        return ''
 
-Chinese = '我的名字叫李明'
-print(mt.mtChinese(Chinese))
+#soup = bs4.BeautifulSoup(get_html('http://www.baidu.com'), 'html.parser')
+#print(get_html('http://poj.org/userstatus?user_id=ooc'))
+print(get_html('http://fanyi.niutrans.com'))
